bb3ce928cee356cb87958bd6ac527b7f64364b59
MahRukh <mahrukhdev@gmail.com>
2023-07-14 12:20:31 +0500

    kotlin basics(functions, arrays, var, val, loops, operators)

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,185 @@
+fun main() {
+
+    var userName: String = "Mah Rukh"
+    userName = "Arsh"
+    //userName = 2
+
+    val age: Int = 10
+    // cannot be changed since the value is const
+    //age = age + 10
+
+
+    println("Hello \$userName! with age $age. In two years, he will be ${age + 2}.  ")
+    print("print without ln")
+    sayHello()
+
+    //more about vals
+    val exampleString = "\"one\ntwo\""
+    println(exampleString)
+    val exampleNum = 2_000_000.12
+    val exampleLong: Long = exampleNum.toLong()
+
+    val exampleChar = '?'
+    val exampleInt: Int = exampleChar.toInt()
+    val exampleBoolean = false
+
+    //PART SIX
+
+    var a = 5
+    val b = 3.0
+
+    println("a = " + a + " b = " + b )
+
+    println("a + b = ${a + b}")
+    println("a - b = ${a - b}")
+    println("a * b = ${a * b}")
+    println("a / b = ${a / b}")
+    println("a % b = ${a % b}")
+
+    println("2 + 2 * 3 = ${2 + 2 * 3}")
+
+    a = a + 2
+
+    a += 2
+    println("a += 2 => $a")
+
+    println("a++ = ${a++}")
+    println("++a = ${++a}")
+    println("-a => ${-a}")
+
+    a -= 2
+    println("a -= 2 => $a")
+
+    a *= 2
+    println("a *= 2 => $a")
+
+    a /= 2
+    println("a /= 2 => $a")
+
+    //PART SEVEN
+    //tools --> Kotlin --> KotlinRepel for testing
+
+    //PART EIGHT
+    //one line comment
+    /**
+     * multiple line comment
+     */
+
+    //PART NINE
+    val names = arrayOf("Jim", "John", "Jenny")
+    val a2 = arrayOf(1, "hello", '?')
+    val a3 = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9),
+        )
+
+    names[0] = "Jermiah"
+    println("first element: ${names[0]}")
+    println("first element letter 3: ${names[0][2]}")
+    println("first element length: ${names[0].length}")
+
+    println("num of elements: ${names.size}")
+    println("last element: ${names.last()}")
+    println(a3[1][1])
+
+
+    //PART TEN
+
+    for (i in names.indices){
+        println("Index: ${i}, Element ${names[i]}")
+    }
+
+    val stringOne = "Hello"
+    for (c in stringOne){
+        println(c)
+    }
+
+    for (i in 1..10) {
+        print(i)
+    }
+println()
+    for (i in 1 until 10) {
+        print(i)
+    }
+println()
+    for (i in 10 downTo 1) {
+        print(i)
+    }
+println()
+    for (i in 1..10 step 2) {
+        print(i)
+    }
+println()
+    for (i in 1.rangeTo(10).step(2)) {
+        print(i)
+    }
+println()
+
+
+    //PART ELEVEN
+
+    val b1 = false
+    println("2>1 is ${2 > 1}")
+    println("!(2>1) is ${!(2 > 1)}")
+    println("2<1 is ${2 < 1}")
+    println("1>=1 is ${1 >= 1}")
+    println("1==1 is ${1 == 1}")
+    println("1==2 is ${1 == 2}")
+    println("1!=2 is ${1 != 2}")
+
+    println("true && false = ${true && false}")
+    println("true || false = ${true || false}")
+
+    val condition = (2>1)||(3>2)&&(2>3)
+    println(condition)
+
+    //PART TWELVE
+
+    var k = 0
+    while(k<5){
+        print(k)
+        //continue
+        //continues to next inclosing loop , moves to check loop then reruns the loop accordingly
+        k++
+        //break
+        //break used to exit from the loop
+    }
+
+    println()
+
+    var l = 0
+    outer@ do {
+        //outer is the label for the loop
+        print(l)
+        l++
+        //break@outer
+    } while(l<5)
+
+    println()
+    //PART THIRTEEN
+    //PART FOURTEEN
+
+    val time = 12
+
+    when (time){
+        in 0..11 -> println("morning")
+        12 -> println("afternoon")
+        13, 14 -> println("nap")
+        else -> println("day")
+    }
+
+    //PART FIFTEEN
+    var name = "Hasham"
+    greet(name)
+    
+}
+
+fun sayHello(){
+    println("Hello, Stella Tech")
+}
+
+//PART FIFTEEN
+fun greet(name: String){
+    println("Hello $name, nice to meet you")
+}
