7bf06e0164c43130eacc76dd0e98cf311f219ae5
MahRukh <mahrukhdev@gmail.com>
2023-07-18 16:47:32 +0500

    abstract class, method, properties

---
+++ src/main/kotlin/AbstractClass.kt
@@ -0,0 +1,17 @@
+fun main(){
+}
+
+abstract class PersonTwo{
+
+    abstract  var name: String //never instantiate
+    abstract fun eat() //abstract doesnt have a body and is by default open
+    open fun getHeight(){} //overriding is optional in case of open
+    fun goToSchool(){}
+}
+
+class Pak: PersonTwo(){
+
+    override var name: String = "dummy_pak_name"
+    override fun eat() {}
+    //should override all abstract methods in subclass
+}
