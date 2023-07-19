99890ff8def994a1bb77e7293a1cbf9903498ad8
MahRukh <mahrukhdev@gmail.com>
2023-07-19 15:13:05 +0500

    high level function with lambda as a parameter

--- src/main/kotlin/HighLevelFunctionsAndLambdas.kt
+++ src/main/kotlin/HighLevelFunctionsAndLambdas.kt
@@ -11,16 +11,19 @@ fun main(){
 
     val test : String = "hello"
     //lambda expression [Function]
-    val myLambda: (Int) -> Unit = { s: Int -> println(s) }
+    val myLambda: (Int, Int) -> Int = { x:Int, y: Int -> x+y }
     program.addTwoNums(2,7,myLambda)
+    program.addTwoNums(2,7, { x , y -> x + y })
+
+    program.addTwoNums(2,4){x,y -> x+y}
 
 }
 
 class Program{
 
-    fun addTwoNums(a:Int, b:Int, action: (Int) -> Unit ){
-        val sum = a+b
-        action(sum)
+    fun addTwoNums(a:Int, b:Int, action: (Int, Int) -> Int ){
+        val sum = action(a,b)
+        println(sum)
     }
     fun addTwoNums(a:Int, b:Int, action: MyInterface){
         val sum = a + b
