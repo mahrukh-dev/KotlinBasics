bbff5518ed7d4eb55a517474b41522a241556459
MahRukh <mahrukhdev@gmail.com>
2023-07-19 17:34:57 +0500

    mutable and immutable lists

--- src/main/kotlin/Collection.kt
+++ src/main/kotlin/Collection.kt
@@ -1,12 +1,11 @@
 fun main(){
     ArrayWork()
-
-
-
+    ImmutableListWork()
+    MutableListWork()
 }
 
 fun ArrayWork(){
-    //Arrays
+    //Arrays - mutable, fixed size
 
     var myArray = Array<Int>(5) {0} //mutable, fixed size
     //Index:    0   1   2   3   4
@@ -22,7 +21,36 @@ fun ArrayWork(){
     for (index in 0..myArray.size-1){
         print(myArray[index])
     }
+}
 
+fun ImmutableListWork(){
+    //lists
+    //immutable, fixed size, read only
+    var list = listOf<String>("n1", "n2", "n3")
+    println( list.get(0))
+}
 
+fun MutableListWork(){
+    //Mutable List - mutable, no fixed size, add/remove
 
+    var mList = mutableListOf<String>()
+    var mList2 = arrayListOf<String>()
+    var mList3 = ArrayList<String>()
+
+    mList.add("l1")
+    mList.add("l2")
+    mList.add("l3")
+
+    mList.remove("l2")
+    mList.add("List2")
+
+    mList[2] = "List 3"
+
+    for(element in mList){
+        print(element)
+    }
+    println()
+    for (index in 0..mList.size-1){
+        print(mList[index])
+    }
 }
