17279e6b8bf8ec23ca0bb322c89a4582cb09cc7f
MahRukh <mahrukhdev@gmail.com>
2023-07-19 13:07:31 +0500

    Static/Objects

---
+++ src/main/kotlin/StaticObject.kt
@@ -0,0 +1,23 @@
+fun main(){
+    //call without creating instance
+    Employee.id = 210910
+    Employee.registerEmp()
+    Employee.myMethod("Mah Rukh")
+}
+
+open class MySuperClass {
+    open fun myMethod(str: String){
+        println("My SUperCLass")
+    }
+}
+object Employee: MySuperClass(){
+    var id: Int = -1
+    fun registerEmp(){
+        println("employee has been registered")
+    }
+
+    override fun myMethod(str: String) { //act as static method
+        super.myMethod(str)
+        println("object Emp: $str")
+    }
+}
